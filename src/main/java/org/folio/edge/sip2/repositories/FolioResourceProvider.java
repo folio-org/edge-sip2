34460648de2fc6a7b82785eb6bc88d3c14529b10
31450518+mreno-EBSCO@users.noreply.github.com <31450518+mreno-EBSCO@users.noreply.github.com>
2019-04-11 12:28:16 -0400

    Reduce duplication/smells

--- src/main/java/org/folio/edge/sip2/repositories/FolioResourceProvider.java
+++ src/main/java/org/folio/edge/sip2/repositories/FolioResourceProvider.java
@@ -21,6 +21,7 @@ import org.apache.logging.log4j.Logger;
  *
  */
 public class FolioResourceProvider implements IResourceProvider<IRequestData> {
+  private static final String HEADER_X_OKAPI_TOKEN = "x-okapi-token";
   private static final Logger log = LogManager.getLogger();
 
   private final String okapiUrl;
@@ -44,25 +45,19 @@ public class FolioResourceProvider implements IResourceProvider<IRequestData> {
   public Future<JsonObject> retrieveResource(IRequestData requestData) {
     final HttpRequest<Buffer> request =
         client.getAbs(okapiUrl + requestData.getPath());
-    for (Map.Entry<String,String> entry : Optional.ofNullable(
-        requestData.getHeaders()).orElse(Collections.emptyMap()).entrySet()) {
-      request.putHeader(entry.getKey(), entry.getValue());
-    }
 
-    if (okapiToken != null) {
-      request.putHeader("x-okapi-token", okapiToken);
-    }
+    setHeaders(requestData.getHeaders(), request);
 
     final Future<JsonObject> future = Future.future();
     request
-        .putHeader("x-okapi-tenant", tenant)
         .expect(ResponsePredicate.SC_OK)
         .expect(ResponsePredicate.JSON)
         .as(BodyCodec.jsonObject())
         .send(ar -> {
           if (ar.succeeded()) {
             // Save the okapi token. There is probably a better way to do this.
-            final String serverToken = ar.result().getHeader("x-okapi-token");
+            final String serverToken = ar.result()
+                .getHeader(HEADER_X_OKAPI_TOKEN);
             if (!serverToken.equals(okapiToken)) {
               okapiToken = serverToken;
             }
@@ -80,25 +75,19 @@ public class FolioResourceProvider implements IResourceProvider<IRequestData> {
   public Future<JsonObject> createResource(IRequestData requestData) {
     final HttpRequest<Buffer> request =
         client.postAbs(okapiUrl + requestData.getPath());
-    for (Map.Entry<String,String> entry : Optional.ofNullable(
-        requestData.getHeaders()).orElse(Collections.emptyMap()).entrySet()) {
-      request.putHeader(entry.getKey(), entry.getValue());
-    }
 
-    if (okapiToken != null) {
-      request.putHeader("x-okapi-token", okapiToken);
-    }
+    setHeaders(requestData.getHeaders(), request);
 
     final Future<JsonObject> future = Future.future();
     request
-        .putHeader("x-okapi-tenant", tenant)
         .expect(ResponsePredicate.SC_CREATED)
         .expect(ResponsePredicate.JSON)
         .as(BodyCodec.jsonObject())
         .sendJsonObject(requestData.getBody(), ar -> {
           if (ar.succeeded()) {
             // Save the okapi token. There is probably a better way to do this.
-            final String serverToken = ar.result().getHeader("x-okapi-token");
+            final String serverToken = ar.result()
+                .getHeader(HEADER_X_OKAPI_TOKEN);
             if (!serverToken.equals(okapiToken)) {
               okapiToken = serverToken;
             }
@@ -120,5 +109,20 @@ public class FolioResourceProvider implements IResourceProvider<IRequestData> {
   @Override
   public Future<JsonObject> deleteResource(IRequestData resource) {
     return null;
+  }
+
+  private void setHeaders(
+      Map<String, String> headers,
+      HttpRequest<Buffer> request) {
+    for (Map.Entry<String,String> entry : Optional.ofNullable(headers)
+        .orElse(Collections.emptyMap()).entrySet()) {
+      request.putHeader(entry.getKey(), entry.getValue());
+    }
+
+    if (okapiToken != null) {
+      request.putHeader(HEADER_X_OKAPI_TOKEN, okapiToken);
+    }
+
+    request.putHeader("x-okapi-tenant", tenant);
   }
 }
