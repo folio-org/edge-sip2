619634a2b33bd8b03c915a10dc639ce5869c8ce1
brichardEbsco <brichard@ebsco.com>
2021-02-26 14:12:39 -0500

    more tests

--- src/main/java/org/folio/edge/sip2/BootstrapVerticle.java
+++ src/main/java/org/folio/edge/sip2/BootstrapVerticle.java
@@ -4,43 +4,55 @@ import io.vertx.config.ConfigRetriever;
 import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Future;
+import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
+import java.util.Objects;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class BootstrapVerticle extends AbstractVerticle {
   private final Logger log = LogManager.getLogger();
-
+  private String deploymentID = null;
+  
   @Override
-  public void start(Future<Void> startFuture) {
+  public void start(Promise<Void> startFuture) {
 
     log.info("Bootstrapping sip2, retrieving config from:\n {}", () -> config().encodePrettily());
-    JsonObject crOptionsJson = config().getJsonObject("configRetrieverOptions");
+    JsonObject crOptionsJson = config().getJsonObject("tenantConfigRetrieverOptions");
     ConfigRetrieverOptions crOptions = new ConfigRetrieverOptions(crOptionsJson);
     ConfigRetriever configRetriever = ConfigRetriever.create(vertx, crOptions);
 
+    configRetriever.listen(change -> {
+      log.info("config change detected for sip2, redeploying MainVerticle");
+      deployMainVerticle(change.getNewConfiguration(), Promise.promise());
+    });    
+
     configRetriever.getConfig(ar -> {
       if (ar.succeeded()) {
-        DeploymentOptions opt = new DeploymentOptions().setConfig(ar.result());
         log.info("Deploying sip2 MainVerticle");
-        vertx.deployVerticle("org.folio.edge.sip2.MainVerticle", opt, mvar -> {
-          if (mvar.succeeded()) {
-            startFuture.complete();
-          } else {
-            startFuture.fail(mvar.cause());
-          }
-        });
+        deployMainVerticle(ar.result(), startFuture);
       } else {
         log.error("Unable to getConfig - {}", ar.cause().getMessage());
         startFuture.fail(ar.cause());
       }
     });
-    
-    configRetriever.listen(change -> {
-      JsonObject newConfig = change.getNewConfiguration();
-      log.info("config change detected for sip2, new config is:\n {}", newConfig.encodePrettily());
-    });
 
+  }
+  
+  private void deployMainVerticle(JsonObject config, Promise<Void> completion) {
+    if (Objects.nonNull(deploymentID)) {
+      vertx.undeploy(deploymentID);
+    }
+    // deploy verticle with config merged with this BootstrapVerticle config so it includes  
+    // port & okapiUrl properties, which originate from BootstrapVerticle config
+    DeploymentOptions opt = new DeploymentOptions().setConfig(config.copy().mergeIn(config()));
+    vertx.deployVerticle("org.folio.edge.sip2.MainVerticle", opt, ar -> {
+      if (ar.succeeded()) {
+        deploymentID = ar.result();
+        completion.complete();
+      } else {
+        completion.fail(ar.cause());
+      }
+    });    
   }
 }
--- src/test/java/org/folio/edge/sip2/api/BootstrapVerticleTests.java
+++ src/test/java/org/folio/edge/sip2/api/BootstrapVerticleTests.java
@@ -1,7 +1,14 @@
 package org.folio.edge.sip2.api;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Verticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.file.FileSystem;
+import io.vertx.core.impl.Deployment;
+import io.vertx.core.impl.VertxInternal;
 import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
@@ -10,38 +17,84 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(VertxExtension.class)
 public class BootstrapVerticleTests {
+
+  private static final String BOOTSTRAP_VERTICLE = "org.folio.edge.sip2.BootstrapVerticle";
+  private static final String MAIN_VERTICLE = "org.folio.edge.sip2.MainVerticle";
+  private static final String SIP2_CONF = "sip2.conf";
+  private static final String SIP2_TENANTS_CONF = "sip2-tenants.conf";
+  private static final String BAD_SIP2_CONF1 = "sip2-bad1.conf";
+  private static final String BAD_SIP2_CONF2 = "sip2-bad2.conf";
+  private static final String DUMMY_PROP = "dummy";
   
   @Test
   public void testHappyPath(Vertx vertx, VertxTestContext tstCtxt) {
 
-    vertx.fileSystem().readFile("fileStoreBootstrap.conf", tstCtxt.succeeding(buffer -> {
+    vertx.fileSystem().readFile(SIP2_CONF, tstCtxt.succeeding(buffer -> {
 
       DeploymentOptions opt = new DeploymentOptions().setConfig(buffer.toJsonObject());
-      vertx.deployVerticle("org.folio.edge.sip2.BootstrapVerticle", opt, tstCtxt.succeeding(id -> {
+      vertx.deployVerticle(BOOTSTRAP_VERTICLE, opt, tstCtxt.succeeding(id -> {
         tstCtxt.completeNow();
       }));
     }));
   }
 
   @Test
-  public void testBadBootstrapConf1(Vertx vertx, VertxTestContext tstCtxt) {
+  public void testBadConf1(Vertx vertx, VertxTestContext tstCtxt) {
 
-    DeploymentOptions opt = new DeploymentOptions().setConfig(new JsonObject());
-    vertx.deployVerticle("org.folio.edge.sip2.BootstrapVerticle", opt, tstCtxt.failing(id -> {
-      tstCtxt.completeNow();
-    }));
-  }
-
-  @Test
-  public void testBadBootstrapConf2(Vertx vertx, VertxTestContext tstCtxt) {
-
-    vertx.fileSystem().readFile("badBootstrap.conf", tstCtxt.succeeding(buffer -> {
+    vertx.fileSystem().readFile(BAD_SIP2_CONF1, tstCtxt.succeeding(buffer -> {
       
       DeploymentOptions opt = new DeploymentOptions().setConfig(buffer.toJsonObject());
-      vertx.deployVerticle("org.folio.edge.sip2.BootstrapVerticle", opt, tstCtxt.failing(id -> {
+      vertx.deployVerticle(BOOTSTRAP_VERTICLE, opt, tstCtxt.failing(id -> {
         tstCtxt.completeNow();
       }));
     }));
+  }
+
+  @Test
+  public void testBadConf2(Vertx vertx, VertxTestContext tstCtxt) {
+
+    vertx.fileSystem().readFile(BAD_SIP2_CONF2, tstCtxt.succeeding(buffer -> {
+      
+      DeploymentOptions opt = new DeploymentOptions().setConfig(buffer.toJsonObject());
+      vertx.deployVerticle(BOOTSTRAP_VERTICLE, opt, tstCtxt.failing(id -> {
+        tstCtxt.completeNow();
+      }));
+    }));
+  }
+
+  @Test
+  public void testSip2RuntimeReload(Vertx vertx, VertxTestContext tstCtxt) {
+    
+    // test runtime config change reload by adding a dummy property to SIP2_TENANTS_CONF
+    // wait 2 seconds and assert that verticle picked up new config with dummy property
+    
+    FileSystem fs = vertx.fileSystem();
+    fs.readFile(SIP2_CONF, tstCtxt.succeeding(buffer -> {
+      
+      DeploymentOptions opt = new DeploymentOptions().setConfig(buffer.toJsonObject());
+      vertx.deployVerticle(BOOTSTRAP_VERTICLE, opt, tstCtxt.succeeding(id -> {
+        
+        JsonObject jo = fs.readFileBlocking(SIP2_TENANTS_CONF).toJsonObject();
+        jo.put(DUMMY_PROP, true);
+        fs.writeFileBlocking(SIP2_TENANTS_CONF, jo.toBuffer());
+        vertx.setTimer(2000, tid -> {
+          JsonObject conf = getVerticle(vertx, MAIN_VERTICLE).config();
+          assertTrue(conf.getBoolean(DUMMY_PROP) == true);
+          tstCtxt.completeNow();
+        });
+      }));
+    }));
+  }
+  
+  private AbstractVerticle getVerticle(Vertx vertx, String name) {
+    Verticle verticle = null;
+    for (String id : vertx.deploymentIDs()) {
+      Deployment d = ((VertxInternal)vertx).getDeployment(id);
+      if (d.verticleIdentifier().equals(name)) {
+        verticle = d.getVerticles().iterator().next();
+      }
+    }    
+    return (AbstractVerticle) verticle;
   }
 
 }
