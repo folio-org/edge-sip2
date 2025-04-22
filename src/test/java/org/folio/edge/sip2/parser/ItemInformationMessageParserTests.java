0b605df91f86184b3740c66df783114d75c7f838
Pavel Filippov <pavel_filippov@epam.com>
2025-04-22 14:54:09 +0300

    SIP2-98: Revert redundant code changes

--- src/test/java/org/folio/edge/sip2/parser/ItemInformationMessageParserTests.java
+++ src/test/java/org/folio/edge/sip2/parser/ItemInformationMessageParserTests.java
@@ -1,11 +1,14 @@
 package org.folio.edge.sip2.parser;
 
+import static java.lang.Character.valueOf;
 import static java.time.temporal.ChronoUnit.SECONDS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import org.folio.edge.sip2.api.support.TestUtils;
+import org.folio.edge.sip2.domain.messages.requests.ItemInformation;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -16,12 +19,15 @@ class ItemInformationMessageParserTests {
   @ParameterizedTest(name = "[{index}] itemIdentifier=''{0}''")
   @ValueSource(strings = { "SomeBook", "  SomeBook", "SomeBook ", "  SomeBook  " })
   void testParse(String itemIdentifier) {
-    var parser = new ItemInformationMessageParser('|', TestUtils.UTCTimeZone);
-    var transactionDate = TestUtils.getOffsetDateTimeUtc().truncatedTo(SECONDS);
-    var formatter = DateTimeFormatter.ofPattern("yyyyMMdd    HHmmss");
-    var transactionDateString = formatter.format(transactionDate);
-    var message = transactionDateString + "AB" + itemIdentifier + "|AOuniversity_id|";
-    var itemInformation = parser.parse(message);
+    ItemInformationMessageParser parser =
+      new ItemInformationMessageParser(valueOf('|'), TestUtils.UTCTimeZone);
+    final OffsetDateTime transactionDate =
+      TestUtils.getOffsetDateTimeUtc().truncatedTo(SECONDS);
+    final DateTimeFormatter formatter = DateTimeFormatter
+      .ofPattern("yyyyMMdd    HHmmss");
+    final String transactionDateString = formatter.format(transactionDate);
+    final ItemInformation itemInformation = parser.parse(
+      transactionDateString + "AB" + itemIdentifier + "|AOuniversity_id|");
 
     assertEquals(transactionDate, itemInformation.getTransactionDate());
     assertEquals("university_id", itemInformation.getInstitutionId());
