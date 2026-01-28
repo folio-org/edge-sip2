2b9e89ac3161ea2088d2526c1cfc0098790791c7
Vadym Zakletskyi <vadym_zakletskyi@epam.com>
2026-01-28 12:43:20 +0200

    SIP-227 Resolve SSL connection failures when using PEM configuration (#237)
    * SIP2-227 Add support for SSL with PEM and P12 certificates in SIP2 configuration
    
    * SIP2-227 Add support for SSL with PEM and P12 certificates in SIP2 configuration
    
    * SIP2-227 Enhance Wiremock support for HTTPS configuration and admin client management
    
    * SIP2-227 Improve WireMock configuration for HTTPS with updated certificate paths and options
    
    * SIP2-227 Add support for PFX and PEM truststores in WebClient configuration
    
    * SIP2-227 Update WireMock stubs
    
    * SIP2-227 Resolve SSL connection failures with PFX configuration and enhance WebClient options
    
    * SIP2-227 Refactor TLS configuration handling in integration tests and WebClient setup

--- src/main/java/org/folio/edge/sip2/utils/WebClientUtils.java
+++ src/main/java/org/folio/edge/sip2/utils/WebClientUtils.java
@@ -4,9 +4,9 @@ import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.net.PemTrustOptions;
+import io.vertx.core.net.PfxOptions;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
-import java.util.Objects;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,6 +16,7 @@ public class WebClientUtils {
   public static final String SYS_PORT = "port";
   public static final String SYS_NET_SERVER_OPTIONS = "netServerOptions";
   public static final String SYS_PEM_KEY_CERT_OPTIONS = "pemKeyCertOptions";
+  public static final String SYS_PFX_KEY_CERT_OPTIONS = "pfxKeyCertOptions";
   public static final String SYS_CERT_PATHS = "certPaths";
   private static final Logger log = LogManager.getLogger();
 
@@ -29,28 +30,42 @@ public class WebClientUtils {
    * @return WebClient
    */
   public static WebClient create(Vertx vertx, JsonObject config) {
+    WebClientOptions options = new WebClientOptions();
     JsonObject netServerOptions = config.getJsonObject(SYS_NET_SERVER_OPTIONS);
-    if (Objects.nonNull(netServerOptions)
-        && netServerOptions.containsKey(SYS_PEM_KEY_CERT_OPTIONS)) {
-      log.info("Creating WebClient with TLS on...");
 
-      JsonArray certPaths = netServerOptions.getJsonObject(SYS_PEM_KEY_CERT_OPTIONS)
-          .getJsonArray(SYS_CERT_PATHS);
-      if (Objects.isNull(certPaths) || certPaths.isEmpty()) {
-        throw new WebClientConfigException("No TLS certPaths were found in config");
+    if (netServerOptions != null) {
+      JsonObject pfx = netServerOptions.getJsonObject(SYS_PFX_KEY_CERT_OPTIONS);
+      JsonObject pem = netServerOptions.getJsonObject(SYS_PEM_KEY_CERT_OPTIONS);
+      if (pfx != null) {
+        options
+            .setSsl(true)
+            .setTrustOptions(new PfxOptions()
+            .setPath(pfx.getString("path"))
+            .setPassword(pfx.getString("password")))
+            .setVerifyHost(false);
+
+        log.info("Creating WebClient with TLS on (using PFX truststore)...");
+        return WebClient.create(vertx, options);
       }
 
-      final PemTrustOptions pemTrustOptions = new PemTrustOptions();
-      certPaths.forEach(entry -> pemTrustOptions.addCertPath((String) entry));
+      if (pem != null) {
+        JsonArray certPaths = pem.getJsonArray("certPaths");
+        if (certPaths == null || certPaths.isEmpty()) {
+          throw new WebClientConfigException("No TLS certPaths were found (pemKeyCertOptions)");
+        }
+        PemTrustOptions trust = new PemTrustOptions();
+        certPaths.forEach(path -> trust.addCertPath((String) path));
+        options
+            .setSsl(true)
+            .setTrustOptions(trust)
+            .setVerifyHost(false);
 
-      final WebClientOptions webClientOptions = new WebClientOptions()
-          .setSsl(true)
-          .setTrustOptions(pemTrustOptions)
-          .setVerifyHost(false); //Hardcoded now. Later it could be configurable using env vars.
-      return WebClient.create(vertx, webClientOptions);
-    } else {
-      log.info("Creating WebClient with TLS off...");
-      return WebClient.create(vertx);
+        log.info("Creating WebClient with TLS on (using PEM truststore)...");
+        return WebClient.create(vertx, options);
+      }
     }
+
+    log.info("Creating WebClient with TLS off...");
+    return WebClient.create(vertx, options);
   }
 }
